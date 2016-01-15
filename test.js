147075397c991ee55389352d528eda64691a65eb
Gabriel Montalvo <gabe.mon5@outlook.com>
2016-01-15 17:42:41 -0400

    adds the files left out from test in the last commit

--- test.js
+++ test.js
@@ -15,16 +15,18 @@ test.serial('generates expected files', async () => {
   helpers.mockPrompt(generator, {
     projectName: 'test',
     databaseUrl: 'mongodb://localhost:27017/testdb"',
-    portNumber: '3000',
-    cli: false
+    portNumber: '3000'
   })
 
   await pify(generator.run.bind(generator))()
 
   assert.file([
+    'controllers',
+    'models',
     '.git',
     '.gitignore',
     'server.js',
+    'config.js',
     'package.json'
   ])
 })
